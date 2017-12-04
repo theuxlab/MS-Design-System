21d04467fe54d6126b16ebbd9e20ac2d86525ffb
snasare <33868545+snasare@users.noreply.github.com>
2017-12-04 09:19:28 -0500

    Focus functionality for AODA compliance

--- components/toast/toast.js
+++ components/toast/toast.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
     fireToast(500);
+    // Use this tab key function according to the situation to focus toast and then close it to tab through other HTML page elements. 
+    // Without closing this toast, you cannot tab through other HTML elements. Use this functionality carefully.     
+    $(document).keyup(function (e) {
+        if (e.keyCode == 9) {
+            if($("body").hasClass("openToast")){                
+                $(".toast").find("p a").focus();
+            }  
+        }             
+    }); // Tab key function ends here.
 
     $('.closeToast').on('click', function() {
         $('.toast').removeClass('open');
